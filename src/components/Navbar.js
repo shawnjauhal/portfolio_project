db9db57ede6e9441a29d6afe3200b9a350576a95
shawnjauhal <shawn.jauhal@gmail.com>
2021-06-02 17:15:39 -0500

    fix some issues with links, navbar.js needs formatting

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,6 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-            <div className="container"> 
                 <a className="navbar-brand" href="#"><img src={logo} alt="logo..." /></a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <FontAwesomeIcon icon={faBars} style={{ color: '#fff' }} />
@@ -19,7 +18,7 @@ const Navbar = () => {
                             <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="">about me</a>
+                            <a className="nav-link" href="#">about me</a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="#">services</a>
@@ -34,7 +33,6 @@ const Navbar = () => {
                             <a className="nav-link" href="#">contact me</a>
                         </li>
                     </ul>
-                </div>
             </div>
         </nav>
     )
